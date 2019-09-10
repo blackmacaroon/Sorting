1b1d7f32ff79c0d82aeb23281cfedd22cb9c54eb
Kayla <6lackmacaroon@gmail.com>
2019-09-10 16:40:47 -0600

    swap 'array' for 'list' for semantics

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,30 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+def selection_sort( list ):
     # loop through n-1 elements - through the indexes, not the values, starting with position zero, "what should be here"
     # loop all the way through each index until you get to the second-to-last...
-    for i in range(0, len(arr) - 1):   # looping through all the values in the dataset
-        for j in range (i + 1, len(arr)):
-            if arr[i] > arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+    for i in range(0, len(list) - 1):   # looping through all the values in the dataset
+        for j in range (i + 1, len(list)):
+            if list[i] > list[j]:
+                list[i], list[j] = list[j], list[i]
         # cur_index = i
         # # to find and assign the smallest
         # smallest_index = cur_index
         # # TO-DO: find next smallest element
         # # check everything to the right of the first index
-        # for j in range(cur_index, len(arr)):   #looping through everything to the right of our new array we want to fill
-        # # j is the new array, starting with your i value
-        # # if the new array number is less than the next, nothing changes, otherwise save the index of the smallest number 
-        #     if arr[j] < arr[smallest_index]:
+        # for j in range(cur_index, len(list)):   #looping through everything to the right of our new list we want to fill
+        # # j is the new list, starting with your i value
+        # # if the new list number is less than the next, nothing changes, otherwise save the index of the smallest number 
+        #     if list[j] < list[smallest_index]:
         #         smallest_index = j 
         # # TO-DO: swap
         # #if the next is larger, variables shift and the next index starts the next round as the temp
         # # this is swapping, temp becomes smallest, smallest becomes current and current becomes temp
-        # ## temp = arr[smallest_index]
-        # ## arr[smallest_index] = arr[cur_index]
-        # ## arr[cur_index] = temp       #aka:
-        # arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+        # ## temp = list[smallest_index]
+        # ## list[smallest_index] = list[cur_index]
+        # ## list[cur_index] = temp       #aka:
+        # list[smallest_index], list[cur_index] = list[cur_index], list[smallest_index]
 
-    return arr
+    return list
 
 
 
@@ -33,32 +33,32 @@ def selection_sort( arr ):
 #compare 1st pair of elements
     #if the RHS is less than the LHS, swap
     #else do nothing
-# iterate through the array, comparing other pairs of adjacent elements
+# iterate through the list, comparing other pairs of adjacent elements
 # if one or more swaps performed, repeat the above process
 
-def bubble_sort( arr ):        #Time Complexity O(n^2)
+def bubble_sort( list ):        #Time Complexity O(n^2)
     # Boolean to check if a swap occured
     swap_occured = True
     while swap_occured:
         #switch swap to false, assuming each time is the last
         swap_occured = False
-        # loop through every item, starting at index zero, through the index in array length -1 (because index starts at 0 and length starts at 1)
-        for i in range(0, len(arr) -1):
+        # loop through every item, starting at index zero, through the index in list length -1 (because index starts at 0 and length starts at 1)
+        for i in range(0, len(list) -1):
             # if i is greater than i+1 (neighbor i), then swap
-            if(arr[i] > arr[i+1]):
-                arr[i], arr[i+1] = arr[i+1], arr[i]  #inline swap, clean syntax
+            if(list[i] > list[i+1]):
+                list[i], list[i+1] = list[i+1], list[i]  #inline swap, clean syntax
                 # if a swap occured, switch back to true to revalidate the loop
                 swap_occured = True
-        # for i in range(0, len(arr)):
-        #     for j in range(0, len(arr)):
-        #         if(arr[i] < arr[j]):
-        #             arr[i], arr[j] = arr[j], arr[i]  #inline swap, clean syntax
+        # for i in range(0, len(list)):
+        #     for j in range(0, len(list)):
+        #         if(list[i] < list[j]):
+        #             list[i], list[j] = list[j], list[i]  #inline swap, clean syntax
         #             swap_occured = True
-        # return arr
-    return arr
+        # return list
+    return list
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort( list, maximum=-1 ):
 
-    return arr
+    return list
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,41 +1,51 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+# TO-DO: complete the helper function below to merge 2 sorted lists
+def merge( listA, listB ):
+    elements = len( listA ) + len( listB )
+    merged_list = [0] * elements
     # TO-DO
-    #loop through each array, compare the first index (that we know is the lowest #) in each array i, j = 0, 0
-    i, j = 0, 0
-    # as long as each index is within the range of their array's length
-    # while i in range (0, len(arrA) -1) or j in range (0, len(arrB) -1):
+    #loop through each list, compare the first index (that we know is the lowest #) in each list i, j = 0, 0
+    i, j, k = 0, 0, 0
+    # as long as each index is within the range of their list's length
+    # while i in range (0, len(listA) -1) or j in range (0, len(listB) -1):
     # aka:
-    while i < len(arrA) or j < len(arrB):
+    while i < len(listA) and j < len(listB):
+        #check if the first element in the first list is smaller than the first element in the second list
+        if listA[i] < listB[j]:
+            #if yes, store first element from first list in merged_list, increase index by 1 for next search 
+            merged_list = listA[i]
+            i = i + 1
+            k = k + 1
+        else:
+            merged_list = listB[j]
+            j = j + 1
+            k = k + 1
 
 
     
-    return merged_arr
+    return merged_list
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort( list ):
     # TO-DO
 
-    return arr
+    return list
 
 
 # STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
+def merge_in_place(list, start, mid, end):
     # TO-DO
 
-    return arr
+    return list
 
-def merge_sort_in_place(arr, l, r): 
+def merge_sort_in_place(list, l, r): 
     # TO-DO
 
-    return arr
+    return list
 
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort( list ):
 
-    return arr
+    return list
